13a312297b0ad5d26aa631ae1f131083d5ecdbd6
unknown <e_sayto@hotmail.com>
2020-04-23 11:10:21 -0300

    Aplicattion Base

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  allowParans: 'avoid'
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+export default App;
